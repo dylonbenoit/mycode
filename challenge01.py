4c5a3c7f47ae7c4efabf7fc9b74dd734b164cb50
dylonbenoit <dylon.s.benoit@gmail.com>
2023-08-15 16:10:16 +0000

    join and split

---
+++ challenge01.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+
+#input asking what is your name
+def main():
+
+    user_input_name = input("What is your name?")
+
+
+#input asking what day of the week is it
+    user_input_day = input('What day of the week is it?')
+
+
+#print statement that reads: Hello, <name>! Happy <day of the week>!
+
+    print("Hello,",user_input_name,"! Happy",user_input_day,"!")
+
+
+main()
+
+
